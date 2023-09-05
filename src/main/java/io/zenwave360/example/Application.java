37e5524b41da7dd31fba41059b84000ca4fad592
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2023-09-05 16:29:19 +0200

    rename spring-boot Application.java

--- src/main/java/io/zenwave360/example/Application.java
+++ src/main/java/io/zenwave360/example/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Zenwave360ExampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Zenwave360ExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
